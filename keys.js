300e24388bcfbc8bd38c3df23d5675d551ca4e3c
bhoang23 <bhoang02@gmail.com>
2018-01-02 20:17:37 -0600

    added files

---
+++ keys.js
@@ -0,0 +1,30 @@
+
+
+
+// spotify
+var spotifyKeys = {
+id: "eaa9340d8a1f4c4eacb57c73cb95d73f",
+secret: "3520473d63d0496b9c2b202acdb7edb1"
+}
+
+
+// twitter
+var twitterKeys = {
+  consumer_key: 'XcEZdvLvY9FOql2tDJsuMG1w5',
+  consumer_secret: 'JwcgGHuEZTBzNxYKaTopy0bbe15SkkX1nZWz2xfTfP2Y9JlXNd',
+  access_token_key: '45071944-hlr4uDEnCROcl7VMUcg7xJjegxcXkR8Kwt8sBB6tD',
+  access_token_secret: '5HgGxauULMN3mrqoBUNmJtzK1gEwccwJFhU9YfYcioltC'
+}
+
+
+// omdb
+var omdbKey = {
+	api_key: "40e9cece"
+}
+
+
+module.exports = {
+	twitter: twitterKeys,
+	spotify: spotifyKeys,
+	omdb: omdbKey
+}	
